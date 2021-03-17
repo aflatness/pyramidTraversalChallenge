7c0e17bf2f89f183e7ed46aa38fbdee958153d1d
aflatness <aflatness@gmail.com>
2021-03-17 14:10:46 -0600

    Main basis of creating pyramid complete

--- pyramidTraverse.js
+++ pyramidTraverse.js
@@ -4,26 +4,51 @@ const Tree = function (val) {
   this.right = null;
 }
 
-const createPyramid = (pyramid) => {
+// [1,2,1,4,1,1];
+//       1
+//      / \
+//     2   1
 
+const createPyramid = (pyramid) => {
+  let previous = null
+  let curr = new Tree(pyramid[0]);
+  let tree = curr;
+  let currNodesRow = [];
+  let ind = 0;
+  let end = 1;
+  let incr = 1;
+
+  while(end <= pyramid.length) {
+    currNodesRow.push(pyramid.slice(ind ,end));
+    let tmp = end;
+    ind = end;
+    incr++;
+    end = tmp + incr;
+  }
+
+  currNodesRow.forEach(row => {
+    row.forEach(node => {
+      //TODO:
+    })
+  })
+  return tree;
 }
+createPyramid([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
 
 const pyramidTraverse = (pyramid, target) => {
   let tree = createPyramid(pyramid);
   let path = '';
-  let found = false;
 
   const traverse = (node, total, currPath) => {
     total *= node.val;
     if (total === target && !node.left && !node.right) {
       path = currPath;
-      found = true;
       return;
     }
     if (node.left && total * node.left.val <= target) {
       traverse(node.left, total, currPath + 'L');
     }
-    if (found) { return };
+    if (path) { return };
     if (node.right && total * node.right.val <= target) {
       traverse(node.right, total, currPath + 'R');
     }
